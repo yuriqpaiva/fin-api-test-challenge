ebe0ae2a685a8507ccbcc681a46cd86abdb72629
Yuri <yuriqpaiva@gmail.com>
2022-11-26 13:02:05 -0300

    Add Create User Use Case test

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,5 +1,23 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
 describe("Create User Use Case", () => {
-  it("should create a new user", () => {
-    console.log("Working!");
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "John Doe",
+      email: "john@example.com",
+      password: "123456",
+    });
+
+    expect(user).toHaveProperty("id");
+    expect(user.name).toEqual("John Doe");
   });
 });
